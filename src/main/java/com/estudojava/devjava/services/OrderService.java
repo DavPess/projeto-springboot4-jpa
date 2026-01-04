c4dc0c4e62045040bf896c15b15ef9daf1463b97
davpess <davidpessoadasilva@hotmail.com>
2026-01-04 12:57:48 -0300

    OrderStatus enum

---
+++ src/main/java/com/estudojava/devjava/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.estudojava.devjava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudojava.devjava.entities.Order;
+import com.estudojava.devjava.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj =  repository.findById(id);
+		return obj.get();
+	}
+	
+}
